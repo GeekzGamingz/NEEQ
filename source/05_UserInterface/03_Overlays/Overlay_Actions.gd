e2a23dcc8d74a1e939e313065178d77db7d991c6
Zeuk <GeekzGamingz@gmail.com>
2024-06-03 02:01:32 -0400

    Animated Button Presses

--- source/05_UserInterface/03_Overlays/Overlay_Actions.gd
+++ source/05_UserInterface/03_Overlays/Overlay_Actions.gd
@@ -11,9 +11,27 @@ var player: CharacterBody2D
 @onready var icon_cancel = $MarginContainer/GridContainer/Cancel_Icon/Icon_Cancel
 #Animation Players
 @onready var icon_player = $AnimationPlayers/IconPlayer
-@onready var button_player = $AnimationPlayers/ButtonPlayer
+#Button Players
+@onready var quick_player = $AnimationPlayers/ButtonPlayers/QuickPlayer
+@onready var interact_player = $AnimationPlayers/ButtonPlayers/InteractPlayer
+@onready var cancel_player = $AnimationPlayers/ButtonPlayers/CancelPlayer
+@onready var travel_player = $AnimationPlayers/ButtonPlayers/TravelPlayer
 #------------------------------------------------------------------------------#
-func _process(_delta):
+#Process Method
+func _process(_delta) -> void:
 	if player == null:
 		player = G.PLAYER
 		if player.name == "Neeq_Overworld": icon_player.play("Overworld")
+#------------------------------------------------------------------------------#
+#Unhandled Input
+func _unhandled_input(event) -> void:
+	#Key Swaps
+	if event.is_action_pressed("action_quick"): quick_player.play("quick_pressed")
+	elif event.is_action_released("action_quick"): quick_player.play("quick")
+	if event.is_action_pressed("action_interact"): interact_player.play("interact_pressed")
+	elif event.is_action_released("action_interact"): interact_player.play("interact")
+	if event.is_action_pressed("action_cancel"): cancel_player.play("cancel_pressed")
+	elif event.is_action_released("action_cancel"): cancel_player.play("cancel")
+	if event.is_action_pressed("action_travel"): travel_player.play("travel_pressed")
+	elif event.is_action_released("action_travel"): travel_player.play("travel")
+#------------------------------------------------------------------------------#
