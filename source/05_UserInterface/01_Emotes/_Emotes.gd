fdaed7cb904cd2a422d223de514d9fe22c4a8e71
Zeuk <GeekzGamingz@gmail.com>
2024-06-18 18:45:40 -0400

    Emote Wheel Progress

--- source/05_UserInterface/01_Emotes/_Emotes.gd
+++ source/05_UserInterface/01_Emotes/_Emotes.gd
@@ -3,18 +3,24 @@ extends Node2D
 #------------------------------------------------------------------------------#
 #Variables
 #Exported Variables
+@export var ui_emote: bool = false
 @export_enum(
 	"Exclaim",
 	"Question",
 	"Interrobang",
 	"Lick",
-	"Lick_Neeq") var emote: String
+	"Lick_Neeq",
+	"Love") var emote: String
 #OnReady Variables
 @onready var anim_player: AnimationPlayer = $AnimationPlayers/AnimationPlayer
 @onready var chat_bubble: Sprite2D = $ChatBubble
 #------------------------------------------------------------------------------#
 #Ready Method
 func _ready() -> void:
+	if ui_emote == true:
+		var anim_names = anim_player.get_animation_list()
+		for anim in anim_names:
+			anim_player.get_animation(anim).loop = true
 	if emote != null:
 		match(emote):
 			"Exclaim": anim_player.play("exclaim")
@@ -22,5 +28,6 @@ func _ready() -> void:
 			"Interrobang": anim_player.play("interrobang")
 			"Lick": anim_player.play("lick")
 			"Lick_Neeq": anim_player.play("lick_neeq")
+			"Love": anim_player.play("love")
 	await anim_player.animation_finished
 	queue_free()
