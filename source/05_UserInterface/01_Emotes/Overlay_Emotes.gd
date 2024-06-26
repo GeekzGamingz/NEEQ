35a4fc7764963f84bd9018f8b91c3a7df94b73d0
Zeuk <GeekzGamingz@gmail.com>
2024-06-25 22:16:14 -0400

    Emote Wheel Logic

--- source/05_UserInterface/01_Emotes/Overlay_Emotes.gd
+++ source/05_UserInterface/01_Emotes/Overlay_Emotes.gd
@@ -2,9 +2,41 @@
 extends Control
 #------------------------------------------------------------------------------#
 #Variables
-var player: CharacterBody2D
+var buttons: Array = []
+var wheel_direction: Vector2 = Vector2.ZERO
 #------------------------------------------------------------------------------#
-func _process(_delta: float) -> void:
-	if player == null:
-		player = G.PLAYER
-		#position = G.PLAYER.position
+#Ready
+func _ready() -> void:
+	var number = 0
+	for button in get_tree().get_nodes_in_group("EmoteButtons"):
+		number += 1
+		match(number):
+			1: button.name = "Exclaim"
+			2: button.name = "Interrobang"
+			3: button.name = "Love"
+			4: button.name = "Button #%s" % [number]
+			5: button.name = "Lick"
+			6: button.name = "Button #%s" % [number]
+			7: button.name = "Button #%s" % [number]
+			8: button.name = "Question"
+		buttons.append(button)
+	print(buttons)
+#------------------------------------------------------------------------------#
+#Process
+func _process(_delta) -> void:
+	visible = Input.is_action_pressed("action_emote")
+	if visible:
+		wheel_direction = Vector2(
+			Input.get_axis("wheel_left", "wheel_right"),
+			Input.get_axis("wheel_up", "wheel_down"))
+		#Verticle
+		if wheel_direction.y < 0 && wheel_direction.x == 0: buttons[0].grab_focus()
+		elif wheel_direction.y > 0 && wheel_direction.x == 0: buttons[4].grab_focus()
+		#Diagnol
+		elif wheel_direction.y < 0 && wheel_direction.x > 0.2: buttons[1].grab_focus()
+		elif wheel_direction.y > 0 && wheel_direction.x > 0.2: buttons[3].grab_focus()
+		elif wheel_direction.y > 0 && wheel_direction.x < 0.2: buttons[5].grab_focus()
+		elif wheel_direction.y < 0 && wheel_direction.x < 0.2: buttons[7].grab_focus()
+		#Horizontal
+		elif wheel_direction.x > 0: buttons[2].grab_focus()
+		elif wheel_direction.x < 0: buttons[6].grab_focus()
---
+++ source/05_UserInterface/01_Emotes/Wheel_Panel.gd
@@ -0,0 +1,13 @@
+#Inherits Control Code
+extends Control
+#------------------------------------------------------------------------------#
+#Variables
+#OnReady Variables
+@onready var button = $TextureButton
+#------------------------------------------------------------------------------#
+#Mouse Focus
+func _on_texture_button_mouse_entered():
+	button.grab_focus()
+#When Focused
+func _on_texture_button_focus_entered():
+	print(button.name)
